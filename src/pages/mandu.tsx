5fc070f808678eafe4591dd7c0245fe54743a594
Ash <ashnamuh@gmail.com>
2020-06-10 17:20:44 +0900

    implement fetching article list feature

--- src/pages/mandu.tsx
+++ src/pages/mandu.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useStore } from 'stores'
+import { useObserver } from 'mobx-react-lite'
 
 const Page: React.FC = () => {
+  const { articleStore } = useStore()
 
-  return (
-    <>Mandu page</>
-  )
+  React.useEffect(() => {
+    articleStore.fetch()
+  }, [articleStore])
+
+  return useObserver(() => (
+    <div>
+      <h1>Mandu page</h1>
+      {articleStore.articles.map(article => <p key={article.slug}>{article.title}</p>)}
+    </div>
+  ))
 }
 
 export default Page
