89954a387a7c59367baee4fe579437e6dfd7b24e
Ash <ashnamuh@gmail.com>
2020-06-10 14:53:41 +0900

    add global style

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
   Link,
   BrowserRouter as Router
 } from 'react-router-dom'
+import GlobalStyled from 'styles/global'
 
 import IndexPage from 'pages/index'
 import AshPage from 'pages/ash'
@@ -13,26 +14,27 @@ import ManduPage from 'pages/mandu'
 export default function App() {
   return (
     <>
-        <Router>
-          <div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/ash">Ash</Link>
-              </li>
-              <li>
-                <Link to="/mandu">Mandu</Link>
-              </li>
-            </ul>
+    <GlobalStyled />
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/ash">Ash</Link>
+            </li>
+            <li>
+              <Link to="/mandu">Mandu</Link>
+            </li>
+          </ul>
 
-            <Switch>
-              <Route exact path="/" component={IndexPage} />
-              <Route exact path="/ash" component={AshPage} />
-              <Route exact path="/mandu" component={ManduPage} />
-            </Switch>
-          </div>
+          <Switch>
+            <Route exact path="/" component={IndexPage} />
+            <Route exact path="/ash" component={AshPage} />
+            <Route exact path="/mandu" component={ManduPage} />
+          </Switch>
+        </div>
         </Router>
     </>
   )
